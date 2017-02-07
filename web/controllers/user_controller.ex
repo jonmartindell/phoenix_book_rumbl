b2f2745bc63aeaf1f879c6386d07e480d20fd3b5
Jonathan Martindell <jmartindell@covermymeds.com>
2017-02-07 11:18:20 -0500

    end of chapter 5

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,5 +1,6 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  plug :authenticate when action in [:index, :show]
   alias Rumbl.User
 
   def new(conn, _params) do
@@ -8,10 +9,11 @@ defmodule Rumbl.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
+    changeset = User.registration_changeset(%User{}, user_params)
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
@@ -20,12 +22,23 @@ defmodule Rumbl.UserController do
   end
 
   def index(conn, _params) do
-    users = Repo.all(Rumbl.User)
+    users = Repo.all(User)
     render conn, "index.html", users: users
   end
 
   def show(conn, %{"id" => id}) do
-    user = Repo.get(Rumbl.User, id)
+    user = Repo.get(User, id)
     render conn, "show.html", user: user
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
   end
 end
